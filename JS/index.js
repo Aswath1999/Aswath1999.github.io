8043aabcd91e8ed0ad9ac9fe22e0517bdbf8bdf3
DESKTOP-O2UQO9I\Aswath <aswath.chandrasekaran@stud.th-deg.de>
2022-03-09 01:55:04 +0530

    o

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
@@ -7,5 +5,3 @@ $(document).ready(function () {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
     });
-
-})
